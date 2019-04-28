4c11bdd68d7568582a13b364ec554d9e33d29cdc
kenan <kenan@dkenan.com>
2019-04-28 11:14:57 +0200

    better mobile view handling

--- assets/javascripts/issue_view_default_columns.js
+++ assets/javascripts/issue_view_default_columns.js
@@ -1,3 +1,4 @@
+// subject and tracker are already included in the first row by default
 $(document).ready(function () {
     $("#available_settings_issue_view_default_columns option[value='tracker']").remove();
     $("#available_settings_issue_view_default_columns option[value='subject']").remove();

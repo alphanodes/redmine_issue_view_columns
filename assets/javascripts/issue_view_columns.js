79ede7752bedecd12ca4b33fc47872567a47abf2
Alexander Meindl <alex@alphanodes.com>
2022-03-03 19:12:34 +0100

    cosmetics

--- assets/javascripts/issue_view_columns.js
+++ assets/javascripts/issue_view_columns.js
@@ -1,5 +1,5 @@
 // subject and tracker are already included in the first row by default
-$(document).ready(function () {
+$(function() {
   $('#available_settings_issue_view_columns option[value="tracker"]').remove();
   $('#available_settings_issue_view_columns option[value="subject"]').remove();
   $('#available_c option[value="tracker"]').remove();

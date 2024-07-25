a7e3890f96bc3303ba16baac2119c6335189d54e
Andrei Munteanu <andrei.munteanu@resonate-mp4.com>
2024-07-25 14:43:36 +0300

    all tests succesfull

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :projects, only: [] do
     resource :issue_view_columns, only: %i[update]
   end
-  resources :issue_view_columns do
+  resources :issue_view_columns, only: [] do
     member do
       patch 'update_collapsed_ids'
     end

da2febb47338782fd6da637edf01c208f36a64dc
Alexander Meindl <a.meindl@alphanodes.com>
2024-11-11 06:59:18 +0100

    Set required version for redmine 6 stable #12953

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_issue_view_columns do
   author_url 'https://alphanodes.com/'
   description 'Customize shown columns in subtasks and related issues on issue page'
   version RedmineIssueViewColumns::VERSION
-  requires_redmine version_or_higher: '5.1' # redmine 6.0 is required, but version is not set in redmine source yet
+  requires_redmine version_or_higher: '6.0'
 
   begin
     requires_redmine_plugin :additionals, version_or_higher: '4.0.0'
